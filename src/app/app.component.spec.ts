d1c129b19d7a037587addc7085e239fac0f8a299
Akhil Aggarwal <akhilaggarwal7790@gmail.com>
2024-07-01 14:12:15 +0530

    First Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AMAZING-ANIMAL-PAINTINGS'`, () => {
+  it(`should have as title 'AMAZINGANIMALPAINTINGS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AMAZING-ANIMAL-PAINTINGS');
+    expect(app.title).toEqual('AMAZINGANIMALPAINTINGS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AMAZING-ANIMAL-PAINTINGS');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AMAZINGANIMALPAINTINGS');
   });
 });
